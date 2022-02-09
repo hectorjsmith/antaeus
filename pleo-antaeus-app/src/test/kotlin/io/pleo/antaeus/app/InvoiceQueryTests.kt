fc014482890abbadc4421e7d311c89778b1c8f86
Hector <hector@hjs.dev>
2022-02-09 11:57:11 +0000

    test: add more tests around the invoice queries
    Add more tests around the invoice query functions in the `AntaeusDal` class
    to ensure they work as expected. New tests ensure that when searching with
    a `maxRetryPaymentTime`, only invoices where the corresponding field is not
    null and before the provided time are returned.
    Pull out duplicate code into separate functions.

--- pleo-antaeus-app/src/test/kotlin/io/pleo/antaeus/app/InvoiceQueryTests.kt
+++ pleo-antaeus-app/src/test/kotlin/io/pleo/antaeus/app/InvoiceQueryTests.kt
@@ -1,10 +1,12 @@
 package io.pleo.antaeus.app
 
+import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
 import org.joda.time.DateTime
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -13,6 +15,16 @@ import java.math.BigDecimal
 import java.util.stream.Stream
 
 class InvoiceQueryTests {
+
+    private fun buildAntaeusDal(): AntaeusDal {
+        val dbFile: File = File.createTempFile("antaeus-db", ".sqlite")
+        return connectToDbAndBuildDal(dbFile)
+    }
+
+    private fun buildEurMoney(): Money {
+        return Money(BigDecimal.valueOf(100), Currency.EUR)
+    }
+
     companion object {
         @JvmStatic
         fun statusAndMaxCreationTimeProviderForQueryTest(): Stream<Arguments> {
@@ -33,19 +45,17 @@ class InvoiceQueryTests {
 
     @ParameterizedTest
     @MethodSource("statusAndMaxCreationTimeProviderForQueryTest")
-    fun given_SqliteDatabaseWithInvoices_When_FetchInvoicesWithStatusAndMaxCreationDate_Then_CorrectInvoicesReturned(
+    fun given_DbWithInvoices_When_FetchInvoicesWithStatusAndMaxCreationDate_Then_CorrectInvoicesReturned(
         statusSet: Set<InvoiceStatus>,
         maxCreationTimeFn: () -> DateTime,
         atLeastOneResultExpected: Boolean
     ) {
         // Assemble
-        val dbFile: File = File.createTempFile("antaeus-db", ".sqlite")
-        val dal = connectToDbAndBuildDal(dbFile)
+        val dal = buildAntaeusDal()
 
-        val money = Money(BigDecimal.valueOf(100), Currency.EUR)
         val customer = dal.createCustomer(Currency.EUR)!!
         for (i in 0 until 20) {
-            dal.createInvoice(money, customer, InvoiceStatus.values().toList().shuffled()[0])
+            dal.createInvoice(buildEurMoney(), customer, InvoiceStatus.values().toList().shuffled()[0])
         }
 
         // Act
@@ -65,5 +75,58 @@ class InvoiceQueryTests {
             invoices.firstOrNull { it.creationTime.isAfter(maxCreationTime) },
             "There should be no invoice with a creation time after the max provided in the query"
         )
+    }
+
+    @Test
+    fun given_DbWithInvoiceWithNullRetryTime_When_FetchInvoicesWithFutureMaxRetryTime_Then_NoInvoicesReturned() {
+        // Assemble
+        val dal = buildAntaeusDal()
+
+        val customer = dal.createCustomer(Currency.EUR)!!
+        val invoice = dal.createInvoice(buildEurMoney(), customer, InvoiceStatus.READY)!!
+        Assertions.assertNull(invoice.retryPaymentTime, "Invoice retry payment time should be null - this is an invalid test")
+
+        // Act
+        val invoices = dal.fetchInvoices(maxRetryPaymentTime = DateTime.now().plusHours(1))
+        println("Found ${invoices.size} invoices")
+
+        // Assert
+        Assertions.assertTrue(invoices.isEmpty(), "Invoice list should be empty")
+    }
+
+    @Test
+    fun given_DbWithInvoiceWithPastRetryTime_When_FetchInvoicesWithFutureMaxRetryTime_Then_InvoiceReturned() {
+        // Assemble
+        val dal = buildAntaeusDal()
+
+        val customer = dal.createCustomer(Currency.EUR)!!
+        val invoice = dal.createInvoice(buildEurMoney(), customer, InvoiceStatus.READY)!!
+        val savedInvoice = dal.updateInvoice(invoice.copy(retryPaymentTime = DateTime.now().minusHours(1)))!!
+        Assertions.assertNotNull(savedInvoice.retryPaymentTime, "Invoice retry payment time should not be null - this is an invalid test")
+
+        // Act
+        val invoices = dal.fetchInvoices(maxRetryPaymentTime = DateTime.now().plusHours(1))
+        println("Found ${invoices.size} invoices")
+
+        // Assert
+        Assertions.assertEquals(1, invoices.size, "A single invoice should be found")
+    }
+
+    @Test
+    fun given_DbWithInvoiceWithFutureRetryTime_When_FetchInvoicesWithPastMaxRetryTime_Then_NoInvoiceReturned() {
+        // Assemble
+        val dal = buildAntaeusDal()
+
+        val customer = dal.createCustomer(Currency.EUR)!!
+        val invoice = dal.createInvoice(buildEurMoney(), customer, InvoiceStatus.READY)!!
+        val savedInvoice = dal.updateInvoice(invoice.copy(retryPaymentTime = DateTime.now().plusHours(1)))!!
+        Assertions.assertNotNull(savedInvoice.retryPaymentTime, "Invoice retry payment time should not be null - this is an invalid test")
+
+        // Act
+        val invoices = dal.fetchInvoices(maxRetryPaymentTime = DateTime.now().minusHours(1))
+        println("Found ${invoices.size} invoices")
+
+        // Assert
+        Assertions.assertTrue(invoices.isEmpty(), "No invoices should be found")
     }
 }
